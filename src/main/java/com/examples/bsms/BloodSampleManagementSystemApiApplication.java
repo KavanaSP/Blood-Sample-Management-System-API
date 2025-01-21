75b26a3175075df467a754b63812aefb8e182af3
KavanaSP <kavanagombe4@gmail.com>
2025-01-21 21:12:23 +0530

    project initiated

---
+++ src/main/java/com/examples/bsms/BloodSampleManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.examples.bsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodSampleManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodSampleManagementSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examples/bsms/BloodSampleManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examples.bsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodSampleManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
